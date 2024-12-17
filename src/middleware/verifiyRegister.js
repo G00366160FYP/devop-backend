155e2df805ab94feb18dd9619b517343cc429353
swordslinger <seanskelton40@gmail.com>
2024-12-17 20:47:09 +0000

    added token verifcation and method to prevent duplicate usernames or emails

---
+++ src/middleware/verifiyRegister.js
@@ -0,0 +1,43 @@
+import DB from "../models/index"
+
+const {user: User} = DB
+
+const checkDupicate = async (req,res, next) => {
+    try{
+        const userNameExists = await User.findOne({
+            where: {
+                username: req.body.username 
+            }
+        })
+
+        if(userNameExists){
+            return res.stats(400).send({
+                message: "Username exists"
+            })
+        }
+
+        const emailExists = await User.findOne({
+            where: {
+                email: req.body.email 
+            }
+        })
+
+        if(emailExists){
+            return res.stats(400).send({
+                message: "Username exists"
+            })
+        }
+
+        next()
+    } catch(error){
+        return res.status(500).send({
+            message:"Internal server error"
+        })
+    }
+}
+
+const verifyRegister = {
+    checkDupicate
+}
+
+export default verifyRegister
