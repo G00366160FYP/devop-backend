8abd4c786ff32f3bee3153ca2f445a08af9ce534
swordslinger <seanskelton40@gmail.com>
2024-12-17 18:37:29 +0000

    Server starting on port 3000

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
+import cors from 'cors'
+
 const app = express();
+const port = 3000
+
+const corsOptions = {
+  origin: 'http://localhost:8081'
+}
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
