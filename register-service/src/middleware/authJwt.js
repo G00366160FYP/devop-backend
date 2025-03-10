a74e432ed640a0ba5e537f478691076f0bbad3de
swordslinger <seanskelton40@gmail.com>
2025-03-10 23:59:29 +0000

    added a git ignore, login-microserive is connected to database and running

---
+++ register-service/src/middleware/authJwt.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import config from "../config/auth.config.js"
+import DB from "../../../config/models/index.js"
+
+const USER = DB.USER
+
+const verifyToken = (req,rees,next) =>{
+    if(!token){
+        return resizeBy.status(403).send({
+            message: "No token"
+        })
+    }
+
+    jwt.verify(token, config.secret, (err, decoded)=>{
+        if(err){
+            return resizeBy.status(401).send({
+                message: "Unauthorized"
+            })
+        }
+        req.userId = decoded.id
+    })
+}
+
+const userValid = async (req,res,next) => {
+    try{
+        const user = await User.findByPk(req.userId)
+
+        if (user){
+            return next
+        }
+
+        return res.status(403).send({
+            message: "Invalid User"
+        })
+    } catch (error){
+        return res.status(500).send({
+            message: "Internal server error"
+        })
+    }
+}
+
+export default {
+    verifyToken,
+    userValid
+}
